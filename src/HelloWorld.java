ddab2ae2a662e75de9104ac0bc379c52c43f06db
sevil32 <aydogansevilfr@gmail.com>
2022-04-08 10:27:32 +0200

    work1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
-    // doing some work here
-    //  it was chanced
+    // Doing awesome work 1
+    // Doing awesome work 2!
+    // Doing awesome work 3!
+
+    //  Awesome work 3 is done and ready to be committed
+    //and ready to be pushed to remote repository
 
 }
